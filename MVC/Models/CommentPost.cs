e179092b716e2f686379bcbb7ed79a4d1e8ee9c9
abhidutz <abhinay@tamu.edu>
2015-07-14 08:04:43 -0400

    added

---
+++ MVC/Models/CommentPost.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class CommentPost
+    {
+
+        public string comment { get; set; }
+       
+        public int emailID { get; set; }
+    }
+}
